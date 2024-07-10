266d0dda93c7de4cce1628c639884bc4a6230557
DP <danila.peshev@kodep.ru>
2024-07-10 11:47:12 +0200

    first attempt to fix

--- currency-converter/src/App.jsx
+++ currency-converter/src/App.jsx
@@ -1,36 +1,44 @@
 import React from 'react';
 
-function MainForm() { 
-  const [data, setData] = React.useState({});
+function Select({data}) {
+  return (
+      Object.values(data.current.data).map((currency) =>
+        <option value={currency.code}>{currency.code}</option>
+      )
+  )
+}
+
+export default function MainForm() {
   const [fromCurrency, setFromCurrency] = React.useState("RUB");
   const [toCurrency, setToCurrency] = React.useState("EUR");
   const [fromPrice, setFromPrice] = React.useState("null");
   const [toPrice, setToPrice] = React.useState("null");
+  const data = React.useRef({});
 
   React.useEffect(() => {
     fetch('https://api.currencyapi.com/v3/latest?apikey=cur_live_v3m1toL9UOcUiy0OylVILzh2A0IsUOFIEtEuAthJ&currencies=')
-  .then((response) => response.json())
-  .then((json) => {
-    setData(json.data);
-    console.log(json.data);
-  })
-  .catch((error) => {
-    console.warn(error);
-    alert("API Access error");
-  })
+      .then((response) => response.json())
+      .then((json) => {
+        data.current = json;
+        console.log(data.current.data);
+      })
+      .catch((error) => {
+        console.error(error);
+        alert("API Access error");
+      })
   }, [])
 
   const onChangeFromPrice = (value) => {
     value = parseFloat(value) < 0.00 ? 0.00 : value;
-    const leftPrice = (value / data[fromCurrency].value);
-    const rightPrice = (leftPrice * data[toCurrency].value); 
+    const leftPrice = (value / data.current.data[fromCurrency].value);
+    const rightPrice = (leftPrice * data.current.data[toCurrency].value); 
     setFromPrice(value);
     setToPrice(rightPrice.toFixed(6));
   }
 
   const onChangeToPrice = (value) => {
     value = parseFloat(value) < 0.00 ? 0.00 : value;
-    const leftPrice = ((data[fromCurrency].value / data[toCurrency].value) * value);
+    const leftPrice = ((data.current.data[fromCurrency].value / data.current.data[toCurrency].value) * value);
     setFromPrice(leftPrice.toFixed(6));
     setToPrice(value);
   }
@@ -56,19 +64,11 @@ function MainForm() {
         <div className="row-currency">
           <p className="you-are-transferring-from-text">Вы переводите из</p>
           <select className="select-to-currency" value={toCurrency} onChange={e => {setToCurrency(e.target.value)}}>
-            {
-              Object.values(data).map((currency) =>
-                <option value={currency.code}>{currency.code}</option>
-              )
-            }
+            <Select data={data}/>
           </select>
           <p className="in-text">в</p>
           <select className="select-from-currency" value={fromCurrency} onChange={e => setFromCurrency(e.target.value)}> 
-            {
-              Object.values(data).map((currency) =>
-                <option value={currency.code}>{currency.code}</option>
-              )
-            }
+            <Select data={data}/>
           </select>
         </div>
         
@@ -81,16 +81,8 @@ function MainForm() {
         </div>
 
         <div className="swap-text">
-          <a onClick={swapCurrencies}>поменять валюты местами</a>
+          <button className="swap-button" onClick={swapCurrencies}>поменять валюты местами</button>
         </div>
     </div>
-  );
-}
-
-export default function App() {
-  return (
-    <>
-    <MainForm/>
-    </>
   );
 }
