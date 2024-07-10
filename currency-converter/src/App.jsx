4bd6c88fdb93b92672e81784add4b78111401c34
DP <danila.peshev@kodep.ru>
2024-07-10 12:13:30 +0200

    first attempt to fix

--- currency-converter/src/App.jsx
+++ currency-converter/src/App.jsx
@@ -1,47 +1,61 @@
 import React from 'react';
 
-function MainForm() { 
-  const [data, setData] = React.useState({});
+function Select({currenciesRef}) {
+  return(
+      Object.values(currenciesRef.current).map((currency) =>
+        <option value={currency.code}>{currency.code}</option>
+      )
+  )
+}
+
+function Input({value, onChangeFunction}) {
+  return(
+    <input type="number" className="to-price" step={1} min={1} 
+          value={value} onChange={value => onChangeFunction(value.target.value)}></input>    
+  )
+}
+
+export default function MainForm() { 
   const [fromCurrency, setFromCurrency] = React.useState("RUB");
   const [toCurrency, setToCurrency] = React.useState("EUR");
   const [fromPrice, setFromPrice] = React.useState("null");
   const [toPrice, setToPrice] = React.useState("null");
+  const currenciesRef = React.useRef({});
 
   React.useEffect(() => {
     fetch('https://api.currencyapi.com/v3/latest?apikey=cur_live_v3m1toL9UOcUiy0OylVILzh2A0IsUOFIEtEuAthJ&currencies=')
-  .then((response) => response.json())
-  .then((json) => {
-    setData(json.data);
-    console.log(json.data);
-  })
-  .catch((error) => {
-    console.warn(error);
-    alert("API Access error");
-  })
+      .then((response) => response.json())
+      .then((json) => {
+        currenciesRef.current = json.data;
+      })
+      .catch((error) => {
+        console.warn(error);
+        alert("API Access error");
+      })
   }, [])
 
   const onChangeFromPrice = (value) => {
     value = parseFloat(value) < 0.00 ? 0.00 : value;
-    const leftPrice = (value / data[fromCurrency].value);
-    const rightPrice = (leftPrice * data[toCurrency].value); 
+    const leftPrice = (value / currenciesRef.current[fromCurrency].value);
+    const rightPrice = (leftPrice * currenciesRef.current[toCurrency].value); 
     setFromPrice(value);
     setToPrice(rightPrice.toFixed(6));
   }
 
   const onChangeToPrice = (value) => {
     value = parseFloat(value) < 0.00 ? 0.00 : value;
-    const leftPrice = ((data[fromCurrency].value / data[toCurrency].value) * value);
+    const leftPrice = ((currenciesRef.current[fromCurrency].value / currenciesRef.current[toCurrency].value) * value);
     setFromPrice(leftPrice.toFixed(6));
     setToPrice(value);
   }
 
-  React.useEffect(() => {
-    onChangeFromPrice(fromPrice);
-  }, [fromCurrency]); 
+  // React.useEffect(() => {
+  //   onChangeFromPrice(fromPrice);
+  // }, [fromCurrency]); 
 
-  React.useEffect(() => {
-    onChangeToPrice(toPrice);
-  }, [toCurrency]); 
+  // React.useEffect(() => {
+  //   onChangeToPrice(toPrice);
+  // }, [toCurrency]); 
 
   function swapCurrencies() {
     const prevCurrency = toCurrency;
@@ -56,41 +70,23 @@ function MainForm() {
         <div className="row-currency">
           <p className="you-are-transferring-from-text">Вы переводите из</p>
           <select className="select-to-currency" value={toCurrency} onChange={e => {setToCurrency(e.target.value)}}>
-            {
-              Object.values(data).map((currency) =>
-                <option value={currency.code}>{currency.code}</option>
-              )
-            }
+            <Select currenciesRef={currenciesRef}/>
           </select>
           <p className="in-text">в</p>
           <select className="select-from-currency" value={fromCurrency} onChange={e => setFromCurrency(e.target.value)}> 
-            {
-              Object.values(data).map((currency) =>
-                <option value={currency.code}>{currency.code}</option>
-              )
-            }
+            <Select currenciesRef={currenciesRef}/>
           </select>
         </div>
         
         <div className="row-values">
-        <input type="number" className="to-price" step={1} min={1} 
-          value={toPrice} onChange={value => onChangeToPrice(value.target.value)}></input>    
+          <Input value={toPrice} onChangeFunction={onChangeToPrice} />
           <p className="equal-text">=</p>
-          <input type="number" className="from-price" step={1} min={1} 
-          value={fromPrice} onChange={value => onChangeFromPrice(value.target.value)}></input>    
+          <Input value={fromPrice} onChangeFunction={onChangeFromPrice} />  
         </div>
 
         <div className="swap-text">
           <a onClick={swapCurrencies}>поменять валюты местами</a>
         </div>
     </div>
-  );
-}
-
-export default function App() {
-  return (
-    <>
-    <MainForm/>
-    </>
   );
 }
